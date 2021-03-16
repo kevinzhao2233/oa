1f531190be0133a38f9e7668ced949bf0f7d274f
kevinzhao2233 <kevin_2233@qq.com>
2021-03-16 17:53:37 +0800

    📦 add some config files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/global.css'
 import App from './App'
+import {observer} from 'mobx-react';
+
+import './styles/reset.css'
+import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // 开发服务端口
+    port: 8383,
+    // 是否开启 https, 但是开启了也没有证书，只在有些功能必须要 https 协议时使用即可
+    https: false
+  },
+  resolve: {
+    // 别名映射，2.0 版本不再需要以 / 结尾了
+    alias: {
+      '/@': path.resolve(__dirname, './src'),
+      '/@comps': path.resolve(__dirname, './src/components'),
+      '/@assets': path.resolve(__dirname, './src/assets'),
+      '/@pages': path.resolve(__dirname, './src/pages')
+    }
+  },
   plugins: [reactRefresh()]
 })
